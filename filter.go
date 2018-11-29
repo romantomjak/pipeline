3b64641c5ad5bdb30ff80048927092f812c5d3fb
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-11-29 20:31:47 +0000

    add reverse filter

--- filter.go
+++ filter.go
@@ -15,3 +15,24 @@ func (ef EchoFilter) Process(in chan Message) chan Message {
 	}()
 	return out
 }
+
+type ReverseFilter struct{}
+
+func (rf ReverseFilter) reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func (rf ReverseFilter) Process(in chan Message) chan Message {
+	out := make(chan Message)
+	go func() {
+		defer close(out)
+		m := <- in
+		m.Body = rf.reverse(m.Body)
+		out <- m
+	}()
+	return out
+}
--- filter_test.go
+++ filter_test.go
@@ -7,14 +7,35 @@ import (
 func TestEchoFilterProcess(t *testing.T) {
 	ef := EchoFilter{}
 	in := make(chan Message)
-	want := Message{
+	m := Message{
 		Body: "Hello World",
 	}
 	go func() {
-		in <- want
+		in <- m
 	}()
+	want := Message{
+		Body: "Hello World",
+	}
 	got := <- ef.Process(in)
 	if want.Body != got.Body {
 		t.Errorf("Echo filter roundtrip error: want='%s', got='%s'", want, got)
+	}
+}
+
+func TestReverseFilterProcess(t *testing.T) {
+	rf := ReverseFilter{}
+	in := make(chan Message)
+	m := Message{
+		Body: "Hello World",
+	}
+	want := Message{
+		Body: "dlroW olleH",
+	}
+	go func() {
+		in <- m
+	}()
+	got := <- rf.Process(in)
+	if want.Body != got.Body {
+		t.Errorf("Reverse filter roundtrip error: want='%s', got='%s'", want, got)
 	}
 }
