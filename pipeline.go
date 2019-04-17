8ad9d3c24c905c29f4b9ee0a75ebc0ed512c5b20
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2019-04-17 21:05:11 +0100

    add comments

--- pipeline.go
+++ pipeline.go
@@ -1,15 +1,18 @@
 package pipeline
 
+// Pipeline keeps references to the start and end of pipeline
 type Pipeline struct {
 	head chan []byte
 	tail chan []byte
 }
 
+// NewPipeline creates a new execution pipeline
 func NewPipeline() *Pipeline {
 	return &Pipeline{}
 }
 
-func (p *Pipeline) Enqueue(filter Filter) {
+// Add adds a new pipeline step
+func (p *Pipeline) Add(filter Filter) {
 	if p.tail == nil {
 		p.head = make(chan []byte)
 		p.tail = filter.Process(p.head)
@@ -18,11 +21,12 @@ func (p *Pipeline) Enqueue(filter Filter) {
 	}
 }
 
+// Process executes the pipeline
 func (p *Pipeline) Process(in []byte) (out []byte) {
 	if p.head == nil {
 		return in
 	}
 	defer close(p.head)
 	p.head <- in
-	return <- p.tail
+	return <-p.tail
 }
