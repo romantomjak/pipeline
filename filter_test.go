3629e3ed7c720697657b93a25aa9be129f7bf437
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2019-04-17 21:05:04 +0100

    move dummy filters to filter package and fix tests

--- filter_test.go
+++ filter_test.go
@@ -2,8 +2,44 @@ package pipeline
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
+
+type echoFilter struct{}
+
+func (ef echoFilter) Process(in chan []byte) chan []byte {
+	out := make(chan []byte)
+	go func() {
+		for m := range in {
+			out <- m
+		}
+		close(out)
+	}()
+	return out
+}
+
+type reverseFilter struct{}
+
+func (rf reverseFilter) reverse(b []byte) []byte {
+	r := make([]byte, len(b))
+	copy(r, b)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return r
+}
+
+func (rf reverseFilter) Process(in chan []byte) chan []byte {
+	out := make(chan []byte)
+	go func() {
+		for m := range in {
+			out <- rf.reverse(m)
+		}
+		close(out)
+	}()
+	return out
+}
 
 func assertBytes(t *testing.T, got, want []byte) {
 	t.Helper()
@@ -13,23 +49,46 @@ func assertBytes(t *testing.T, got, want []byte) {
 }
 
 func TestEchoFilterProcess(t *testing.T) {
-	ef := EchoFilter{}
-	in := make(chan []byte)
-	go func() {
-		in <- []byte("Hello World")
-	}()
-	got := <- ef.Process(in)
-	want := []byte("Hello World")
-	assertBytes(t, got, want)
+
+	t.Run("send once", func(t *testing.T) {
+		ef := echoFilter{}
+		in := make(chan []byte)
+		go func() {
+			in <- []byte("Hello World")
+			close(in)
+		}()
+		got := <-ef.Process(in)
+		want := []byte("Hello World")
+		assertBytes(t, got, want)
+	})
+
+	t.Run("send many", func(t *testing.T) {
+		ef := echoFilter{}
+		in := make(chan []byte)
+		expect := 3
+		go func() {
+			for i := 0; i < expect; i++ {
+				in <- []byte(fmt.Sprintf("Hello World #%d", i))
+			}
+			close(in)
+		}()
+		out := ef.Process(in)
+		for i := 0; i < expect; i++ {
+			want := []byte(fmt.Sprintf("Hello World #%d", i))
+			got := <-out
+			assertBytes(t, got, want)
+		}
+	})
 }
 
 func TestReverseFilterProcess(t *testing.T) {
-	rf := ReverseFilter{}
+	rf := reverseFilter{}
 	in := make(chan []byte)
 	go func() {
 		in <- []byte("Hello World")
+		close(in)
 	}()
 	want := []byte("dlroW olleH")
-	got := <- rf.Process(in)
+	got := <-rf.Process(in)
 	assertBytes(t, got, want)
 }
--- pipeline_test.go
+++ pipeline_test.go
@@ -4,39 +4,6 @@ import (
 	"testing"
 )
 
-type echoFilter struct{}
-
-func (ef echoFilter) Process(in chan []byte) chan []byte {
-	out := make(chan []byte)
-	go func() {
-		defer close(out)
-		m := <-in
-		out <- m
-	}()
-	return out
-}
-
-type reverseFilter struct{}
-
-func (rf reverseFilter) reverse(b []byte) []byte {
-	r := make([]byte, len(b))
-	copy(r, b)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return r
-}
-
-func (rf reverseFilter) Process(in chan []byte) chan []byte {
-	out := make(chan []byte)
-	go func() {
-		defer close(out)
-		m := <-in
-		out <- rf.reverse(m)
-	}()
-	return out
-}
-
 func TestEmptyPipelineReturnsSameMessage(t *testing.T) {
 	p := NewPipeline()
 	want := []byte("Hello World")
